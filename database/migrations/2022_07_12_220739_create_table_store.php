1673e47dcc116902467c42a7dec2e3facb05b162
Wilson <wilsonevangelista22.we@gmail.com>
2022-07-15 06:03:01 -0300

    Seed e Factory

--- database/migrations/2022_07_12_220739_create_table_store.php
+++ database/migrations/2022_07_12_220739_create_table_store.php
@@ -36,6 +36,6 @@ class CreateTableStore extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_store');
+        Schema::dropIfExists('stores');
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,7 +12,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('users')->insert(
+        /* \DB::table('users')->insert(
             [
                 'name' => 'Administrador',
                 'email' => 'adm@gmail.com',
@@ -19,6 +20,7 @@ class UsersTableSeeder extends Seeder
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
             ]
-            );
+            ); */
+            factory(User::class, 40)->create();
     }
 }
